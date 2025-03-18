aa61b8c9db57bc7790755312f69a3c764e594983
867-Jige <panyuji@canway.net>
2025-03-18 17:47:15 +0800

    opt: 账号有效期优化

--- src/pages/src/views/organization/details/InputComponents.vue
+++ src/pages/src/views/organization/details/InputComponents.vue
@@ -30,7 +30,6 @@
           @phone="(val) => item.isError = val" />
         <template v-else-if="item.key.includes(dateKey)">
           <bk-date-picker
-            ext-cls="ext-cls"
             v-model="item.value"
             @change="changSelect"
             format="yyyy-MM-dd"
@@ -98,6 +97,7 @@ export default {
   },
   computed: {
     shortcuts() {
+      console.log(this.$store.state.passwordValidDaysList);
       const dayTime = 24 * 60 * 60 * 1000;
       const nowTime = new Date().getTime();
       return this.$store.state.passwordValidDaysList.map((item) => {
@@ -209,8 +209,5 @@ export default {
 }
 .tips-style {
   color: #ea3636;
-}
-.ext-cls {
-  width: 100%;
 }
 </style>
